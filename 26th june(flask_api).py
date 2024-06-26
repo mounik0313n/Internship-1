31584e8a3bef18627713c5c967fded803616002a
Javvaji Mounik prasad <112876521+mounik0313n@users.noreply.github.com>
2024-06-26 23:35:21 +0530

    Update 26th june(flask_api).py

--- 26th june(flask_api).py
+++ 26th june(flask_api).py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'jp2335066@gmail.com'
-app.config['MAIL_PASSWORD'] = 'pqpxzuubeqmdpgda'  # Replace with your actual Gmail password or app password
+app.config['MAIL_USERNAME'] = 'user mail id'
+app.config['MAIL_PASSWORD'] = 'XXXXXXXXXXXX'  # Replace with your actual Gmail password or app password
 
 # MySQL database connection
 def get_db_connection():

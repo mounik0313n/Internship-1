370fc6cd18aad46712474211d31247c9bd531cda
Javvaji Mounik prasad <112876521+mounik0313n@users.noreply.github.com>
2024-06-26 23:34:43 +0530

    Update final.py

--- bulkemail copy/final.py
+++ bulkemail copy/final.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'jp2335066@gmail.com'
-app.config['MAIL_PASSWORD'] = 'pqpxzuubeqmdpgda'  # Replace with your actual Gmail password or app password
+app.config['MAIL_USERNAME'] = 'user mail'
+app.config['MAIL_PASSWORD'] = 'app password'  # Replace with your actual Gmail password or app password
 
 # MySQL database connection
 def get_db_connection():
